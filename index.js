08c2decd22ef278a0163b5ccca5609efdf008a7e
Jonathan Rawlings <jonraw8@gmail.com>
2022-05-22 13:04:47 -0700

    cleaned up with objects

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 
-document.getElementById("hero").innerHTML = 
-`<div class="character-card">
-    <h4 class="name"> Wizard </h4>
-    <img class="avatar" src="images/wizard.png"/>
-    <p class="health">health: <b> 60 </b></p>
-    <div class="dice-container"><div class="dice"> 6 </div></div>
-</div>`; 
+const hero = {
+    id : "hero",
+    name : "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceRoll: 6
+}
+const monster = {
+    id: "monster",
+    name: "orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceRoll: 4
+}
 
-document.getElementById("monster").innerHTML = 
-`<div class="character-card">
-    <h4 class="name"> Orc </h4>
-    <img class="avatar" src="images/orc.png"/>
-    <p class="health">health: <b> 10 </b></p>
-    <div class="dice-container"><div class="dice"> 4 </div></div>
-</div>`; 
 
+
+function renderCharacters(data){
+    document.getElementById(data.id).innerHTML = 
+        `<div class="character-card">
+            <h4 class="name">${data.name}</h4>
+            <img class="avatar" src="${data.avatar}"/>
+            <p class="health">health: <b>${data.health} </b></p>
+            <div class="dice-container"><div class="dice">${data.diceRoll}</div></div>
+        </div>`; 
+}
+
+renderCharacters(hero)
+renderCharacters(monster)
